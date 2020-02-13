133804aaeb52060cf26e338b43c5d64a4ecc3577
tvaz <victoriavaz24@gmail.com>
2020-02-13 14:12:34 -0800

    Can add posts now

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, validators
 
 app = Flask(__name__)
 
@@ -16,12 +17,27 @@ class Post(db.Model):
         return 'Post: "{}"'.format(self.title)
 #db.create_all()
 
+class NewPostForm(Form):
+    title = StringField('Title', [validators.length(min=1, max=120)])
+    body = StringField('Body', [validators.length(min=1, max=2000)]) 
+
 @app.route('/')
 def index():
     posts = Post.query.all()
+    print(posts)
     return render_template("index.html", posts=posts)
 
-@app.route('/new')
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    form = NewPostForm(request.form)
+    if request.method == 'POST' and form.validate():
+        post = Post(title=form.title.data, body=form.body.data)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("new.html", form=form)
+
+@app.route('/create')
 def create():
     return index()
 
