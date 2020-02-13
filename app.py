284e71db5f531ca11c8761978c3158d4ab6d3839
tvaz <victoriavaz24@gmail.com>
2020-02-13 15:29:34 -0800

    All functionality requested is now implemented.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+"""
+A simple implementation of a blogging application.
+
+Tori Vaz 2020
+"""
+
 from flask import Flask, render_template, request, redirect, url_for
 from forms import NewPostForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 class Post(db.Model):
     id = db.Column('id', db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(120), nullable=False)
@@ -18,15 +23,27 @@ class Post(db.Model):
     def __repr__(self):
         return 'Post: "{}"'.format(self.title)
 
-db.create_all()
-
 @app.route('/')
 def index():
+    """
+    Main page for application. Shows a list of all posts.
+    """
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
+@app.route('/post/<id>')
+def post(id):
+    """
+    Shows details for a given post.
+    """
+    post = Post.query.filter_by(id=id).one()
+    return render_template("post.html", post=post)
+
 @app.route('/new', methods=['GET', 'POST'])
 def new_post():
+    """
+    Creates a new post.
+    """
     form = NewPostForm(request.form)
     if request.method == 'POST' and form.validate():
         post = Post(title=form.title.data, body=form.body.data)
@@ -37,12 +54,18 @@ def new_post():
 
 @app.route('/del/<id>')
 def delete(id):
+    """
+    Deletes a post and returns to the main page.
+    """
     post = Post.query.filter_by(id=id).delete()
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/edit/<id>', methods=['GET', 'POST'])
 def update(id):
+    """
+    Updates the post and returns to the post details view.
+    """
     post = Post.query.filter_by(id=id).one()
     form = NewPostForm(obj=post)
 
@@ -52,6 +75,5 @@ def update(id):
             post.title = form.title.data
             post.body = form.body.data
             db.session.commit()
-            return redirect(url_for('index'))
-
+            return redirect(url_for('post', id=id))
     return render_template("edit.html", form=form)
